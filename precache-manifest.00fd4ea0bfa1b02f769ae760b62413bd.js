61202d52b634f8b1364b4909202d8acb2973d504
Bierwurst <35297707+Bierwurst@users.noreply.github.com>
2019-05-14 19:53:07 +0200

    added brewery project

---
+++ precache-manifest.00fd4ea0bfa1b02f769ae760b62413bd.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "beb74282a9da895967a0a4dd5b5b59a4",
+    "url": "/my-representation/static/media/mem-cards.beb74282.gif"
+  },
+  {
+    "revision": "b1ea455e279467786b73",
+    "url": "/my-representation/static/css/main.a156e13c.chunk.css"
+  },
+  {
+    "revision": "8c5475719e887656cd0b",
+    "url": "/my-representation/static/js/runtime~main.8c547571.js"
+  },
+  {
+    "revision": "e696ed06d87aa643389f",
+    "url": "/my-representation/static/js/2.e696ed06.chunk.js"
+  },
+  {
+    "revision": "a3d00201d6f35c942d6a0a63e0284fda",
+    "url": "/my-representation/static/media/a-logo.a3d00201.svg"
+  },
+  {
+    "revision": "e19250ec5903f305a331637fb9ff815e",
+    "url": "/my-representation/static/media/welcomeScreen.e19250ec.mp4"
+  },
+  {
+    "revision": "e6dfabb4756bcf8062d6a9028095e9a6",
+    "url": "/my-representation/static/media/portrait.e6dfabb4.png"
+  },
+  {
+    "revision": "c3d2dcb401cf886d5e5fc990af4510c5",
+    "url": "/my-representation/static/media/squirrel-1.c3d2dcb4.png"
+  },
+  {
+    "revision": "f029ea66b0ea9c4e42c2242933ef5e0c",
+    "url": "/my-representation/static/media/squirrel-2.f029ea66.png"
+  },
+  {
+    "revision": "c21e2cc9f832e677e9158d857c028849",
+    "url": "/my-representation/static/media/squirrel-3.c21e2cc9.png"
+  },
+  {
+    "revision": "57a6fa73e50e4525529fb29a85afa488",
+    "url": "/my-representation/static/media/squirrel-4.57a6fa73.png"
+  },
+  {
+    "revision": "df5a7e02c1fdea39e500ac8eaa926cdb",
+    "url": "/my-representation/static/media/squirrel-5.df5a7e02.png"
+  },
+  {
+    "revision": "485d8d3f0b98d4cdce08006b864c0018",
+    "url": "/my-representation/static/media/squirrel-6.485d8d3f.png"
+  },
+  {
+    "revision": "f68dfa6bd0eaebe01897fed7fe4c6d54",
+    "url": "/my-representation/static/media/notlogo.f68dfa6b.svg"
+  },
+  {
+    "revision": "b1ea455e279467786b73",
+    "url": "/my-representation/static/js/main.b1ea455e.chunk.js"
+  },
+  {
+    "revision": "8cb436bc635fb813ab6420d4cdc89a4b",
+    "url": "/my-representation/static/media/01_Titel_Signika.8cb436bc.png"
+  },
+  {
+    "revision": "97ffe91d2f7bb308e160fb7333bb3e49",
+    "url": "/my-representation/static/media/02-sama-nozug.97ffe91d.png"
+  },
+  {
+    "revision": "1734384e67ec813e76610bb2927026d5",
+    "url": "/my-representation/static/media/03-sama-zug.1734384e.png"
+  },
+  {
+    "revision": "5bc787b5712f9697ac29d55e8571b3e3",
+    "url": "/my-representation/static/media/04-aufzug.5bc787b5.png"
+  },
+  {
+    "revision": "38e701fcbe59c11b27f35ec9c36fe78a",
+    "url": "/my-representation/static/media/05-alex.38e701fc.png"
+  },
+  {
+    "revision": "2332568065b11716b9646f15a1506cf2",
+    "url": "/my-representation/static/media/06-regional.23325680.png"
+  },
+  {
+    "revision": "9392bc569829c9073da4f0f53f7cd707",
+    "url": "/my-representation/static/media/07-blauer.9392bc56.png"
+  },
+  {
+    "revision": "8eb1d1b2caecd3dff74e1e2ad4a36e63",
+    "url": "/my-representation/static/media/08-gueter.8eb1d1b2.png"
+  },
+  {
+    "revision": "0813a0a61bb6ae6534e02cf27809f174",
+    "url": "/my-representation/static/media/09-sbahn.0813a0a6.png"
+  },
+  {
+    "revision": "467ad0bb304511115f6ebad3711fd0dd",
+    "url": "/my-representation/static/media/10-ICE.467ad0bb.png"
+  },
+  {
+    "revision": "28d7530c580548bee139c27f2ea5e325",
+    "url": "/my-representation/static/media/11-notrain.28d7530c.png"
+  },
+  {
+    "revision": "7893098c1d460a9b4d980085826d260f",
+    "url": "/my-representation/static/media/12-toytrain.7893098c.png"
+  },
+  {
+    "revision": "8531c83e3557114b5dfe37dfc27c80f2",
+    "url": "/my-representation/static/media/13inBed.8531c83e.png"
+  },
+  {
+    "revision": "8fca13d1527caf021ea17d2a6247bb6a",
+    "url": "/my-representation/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d0cd8930a73f2f9a2a77095169895ad1.js"
+  "/my-representation/precache-manifest.00fd4ea0bfa1b02f769ae760b62413bd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/my-representation/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
